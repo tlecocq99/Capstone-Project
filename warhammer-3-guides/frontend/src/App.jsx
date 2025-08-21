d21c7e0c8b7c22297273cff17aa44e78250896a2
Thomas Lecocq <tlecocq99@gmail.com>
2025-08-21 01:10:31 -0500

    addition of UserProvider tabs for global login context

--- warhammer-3-guides/frontend/src/App.jsx
+++ warhammer-3-guides/frontend/src/App.jsx
@@ -5,16 +5,19 @@ import FactionsListPage from "./pages/FactionsListPage";
 import FactionDetailPage from "./pages/FactionDetailPage";
 import FeedbackPage from "./pages/FeedbackPage";
 import SignupPage from "./pages/SignupPage";
+import { UserProvider } from "./contexts/UserContext";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/factions" element={<FactionsListPage />} />
-      <Route path="/factions/:slug" element={<FactionDetailPage />} />
-      <Route path="/feedback" element={<FeedbackPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-    </Routes>
+    <UserProvider>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/factions" element={<FactionsListPage />} />
+        <Route path="/factions/:slug" element={<FactionDetailPage />} />
+        <Route path="/feedback" element={<FeedbackPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </UserProvider>
   );
 }
 export default App;
