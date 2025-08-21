5c400ea3821d34072761aa78fdb90638857a4891
Thomas Lecocq <tlecocq99@gmail.com>
2025-08-20 22:33:16 -0500

    addition of saved factions with logged in userstate

--- warhammer-3-guides/backend/controllers/authController.js
+++ warhammer-3-guides/backend/controllers/authController.js
@@ -1,3 +1,30 @@
+exports.saveFaction = async (req, res) => {
+  const { username, factionSlug } = req.body;
+  try {
+    const user = await User.findOneAndUpdate(
+      { username },
+      { $addToSet: { savedFactions: factionSlug } },
+      { new: true }
+    );
+    res.json({ success: true, savedFactions: user.savedFactions });
+  } catch (err) {
+    res.status(400).json({ success: false, message: err.message });
+  }
+};
+
+exports.unsaveFaction = async (req, res) => {
+  const { username, factionSlug } = req.body;
+  try {
+    const user = await User.findOneAndUpdate(
+      { username },
+      { $pull: { savedFactions: factionSlug } },
+      { new: true }
+    );
+    res.json({ success: true, savedFactions: user.savedFactions });
+  } catch (err) {
+    res.status(400).json({ success: false, message: err.message });
+  }
+};
 const User = require("../models/User");
 
 exports.signup = async (req, res) => {
