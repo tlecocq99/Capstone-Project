6bad40c0f67b9d41f8e1e5783018f47c639ca701
Thomas Lecocq <tlecocq99@gmail.com>
2025-08-20 22:33:48 -0500

    addition of save faction and unsave faction

--- warhammer-3-guides/backend/routes/authRoutes.js
+++ warhammer-3-guides/backend/routes/authRoutes.js
@@ -4,5 +4,7 @@ const authController = require("../controllers/authController");
 
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
+router.post("/save-faction", authController.saveFaction);
+router.post("/unsave-faction", authController.unsaveFaction);
 
 module.exports = router;
