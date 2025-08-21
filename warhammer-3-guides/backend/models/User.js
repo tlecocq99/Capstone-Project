a5d2a7d5a42ea8f63f765d7a0a4f305b606f5f71
Thomas Lecocq <tlecocq99@gmail.com>
2025-08-20 22:33:36 -0500

    added in the ability to create an account as well as login

--- warhammer-3-guides/backend/models/User.js
+++ warhammer-3-guides/backend/models/User.js
@@ -1,7 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    birthYear: { type: Number, required: true }
+  username: { type: String, required: true, unique: true },
+  birthYear: { type: Number, required: true },
+  savedFactions: [{ type: String }], // Array of faction slugs
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
